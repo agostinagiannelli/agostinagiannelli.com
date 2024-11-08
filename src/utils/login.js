99e82d7eeda889438d9c77caba01da6cdc0989bd
Agostina Giannelli <agostinagiannelli@gmail.com>
2024-11-09 00:03:21 +0100

    Full web update v2

---
+++ src/utils/login.js
@@ -0,0 +1,31 @@
+document.addEventListener("astro:page-load", () => {
+    const form = document.getElementById("form");
+    const result = document.getElementById("result");
+
+    form.addEventListener("submit", async function (e) {
+        e.preventDefault();
+        form.classList.add("was-validated");
+
+        if (!form.checkValidity()) {
+            form.querySelectorAll(":invalid")[0].focus();
+            return;
+        }
+
+        const enteredPassword = document.getElementById("password").value;
+        const correctPassword = "123";
+
+        if (enteredPassword === correctPassword) {
+            sessionStorage.setItem("authorized", "true");
+            window.location.href = "/case-studies";
+        } else {
+            result.classList.add("text-red-400");
+            result.innerHTML = "Incorrect password. Please try again.";
+        }
+
+        setTimeout(() => {
+            form.reset();
+            form.classList.remove("was-validated");
+            result.innerHTML = "";
+        }, 3000);
+    });
+});
