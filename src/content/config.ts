6f64d3dd11f3e97f851c9ccc95998d436311722e
Agostina Giannelli <agostinagiannelli@gmail.com>
2024-11-11 19:27:09 +0100

    Badge link fixed

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,5 @@
-// 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
 
-// 2. Define your collection(s)
 const caseCollection = defineCollection({
   schema: z.object({
     title: z.string(),
@@ -25,39 +23,6 @@ const caseCollection = defineCollection({
   }),
 });
 
-const blogCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    title: z.string(),
-    snippet: z.string(),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('Astroship'),
-    category: z.string(),
-    tags: z.array(z.string()),
-  }),
-});
-
-const teamCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    name: z.string(),
-    title: z.string(),
-    avatar: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    publishDate: z.string().transform(str => new Date(str)),
-  }),
-});
-
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
 export const collections = {
   'case': caseCollection,
-  'blog': blogCollection,
-  'team': teamCollection,
 };
